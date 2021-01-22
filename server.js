1bfb265a1a1575947fa8b7558246bac460c9bbc0
bhaveshpp <bhaveshpp11@gmail.com>
2021-01-22 23:49:54 +0530

    add first get routs

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [{name: 'Bhavesh'},{name: 'Akash'}];
+
+app.get('/users',function(req,res){
+  res.json({
+  	success: true,
+	message: 'Successfully get users.',
+	users: mockUserData
+  });
+});
 
 app.listen(8000,function(){
   console.log("Server is Running");
 });
-
